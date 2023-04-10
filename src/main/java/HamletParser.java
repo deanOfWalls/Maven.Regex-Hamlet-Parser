6d1df41f90d040133ca8d9850f8ba2859742a2ae
deanOfWalls <deanofwalls@gmail.com>
2023-04-10 18:03:07 -0400

    checkpointing work

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,37 @@ public class HamletParser {
         return hamletData;
     }
 
+
+    public void changeHamletToLeon(){
+        Pattern pattern = Pattern.compile("HAMLET");
+        Matcher matcher = pattern.matcher(hamletData);
+        StringBuffer sb = new StringBuffer();
+        String replacementText = "LEON";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText);
+                    }
+    matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+        Pattern pattern2 = Pattern.compile("Hamlet");
+        Matcher matcher2 = pattern2.matcher(hamletData);
+        StringBuffer sb2 = new StringBuffer();
+        String replacementText2 = "Leon";
+        while(matcher2.find()){
+            matcher2.appendReplacement(sb2, replacementText2);
+        }
+        matcher2.appendTail(sb2);
+        hamletData = sb2.toString();
+    }
+
+    public void changeHoratioToTariq() {
+    }
+
+    public int findHoratio() {
+        return -1;
+    }
+
+    public int findHamlet() {
+        return -1;
+    }
 }
